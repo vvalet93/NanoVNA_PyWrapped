468f5fbd60b5c773b4c497fea8cadb78b012da31
Valerii Verozub <verozub.vj@gmail.com>
2022-10-25 22:10:23 -0500

    Compilation works now

--- xavna_controller.cpp
+++ xavna_controller.cpp
@@ -6,7 +6,7 @@ int main(){
     auto devices = VNADevice::findDevices();
     std::cout <<"Next devices have been found:" << std::endl;
     for (auto device : devices){
-        std::cout<<device<<std::endl;
+       std::cout<<device<<std::endl;
     }
     
     return 0;
